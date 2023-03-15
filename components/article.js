5aab630f8a53a12b963e660a33b4ad568be2ce5b
Antonio <aadresgarza@outlook.es>
2023-03-15 19:04:39 +0100

    Selección de elemento y apertura de modal

--- components/article.js
+++ components/article.js
@@ -4,28 +4,54 @@ import {getRequestData} from '/services/data.service.js';
 class article extends HTMLElement {
   constructor() {
     super();
-  
+  }
+  /**
+  * Variable compartida para almacenar el listado de la respuesta del servicio
+  */
+  static allArticles;
+  /**
+  * Variable compartida para almacenar el elemento seleccionado, filtrado de la respuesta del servicio
+  */
+  static selectedArticle;
+
+  connectedCallback() {
+    /**
+     * Pedimos los datos de los artículos al servicio
+     */
     let getFetch = async () => {
       let res = await getRequestData();
+      window.allArticles = await getRequestData();
       var count=1;
       res.forEach(element => {
         if ( count ===1 || ( count % 8 ) == 0  ) {
-           this.innerHTML += `<article style='background-image:url("${element.url}")' class="item big-item" alt="IMG" id=${count}" ></article>`;
+           this.innerHTML += `<article style='background-image:url("${element.url}")' class="item big-item" alt="IMG" id="${count}" data-bs-toggle="modal" data-bs-target="#exampleModal" ></article>`;
+           
            count++
          } else {
-           this.innerHTML += `<article style='background-image:url("${element.url}")' class="item" alt="IMG" class="regular-item" id=${count}" ></article>`;
+           this.innerHTML += `<article style='background-image:url("${element.url}")' class="item" alt="IMG" class="regular-item" id="${count}" data-bs-toggle="modal" data-bs-target="#exampleModal"></article>`;
            count++
          }
         });
+      //Añadimos el evento 
+      for(var i=1; i < count; i++){
+        var myModal = document.getElementById(`${i}`);
+        myModal.addEventListener("click", modifyText, false);
+        myModal.myParam = `${i}`;
+      }
    }
    getFetch();
-    
-  }
   
- 
+    
 
-
-  connectedCallback() {
+   
+   function modifyText(evt){
+      var article = evt.currentTarget.myParam;
+      window.selectedArticle = window.allArticles.find(element => element.id == article);
+      alert(JSON.stringify(window.selectedArticle))
+      
+    }
+    
+   
  } 
 
 
