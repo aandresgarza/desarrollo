aec9f3500cb9c38485d5d164f8804eeb92c693af
Antonio <aadresgarza@outlook.es>
2023-03-23 19:00:35 +0100

    carosusel

--- components/cs-modal.js
+++ components/cs-modal.js
@@ -31,29 +31,11 @@ class Modal extends HTMLElement {
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body container">
-          <section class="container">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="${selected.images[1]}" class="w-auto d-inline"  alt="IMG" >
-                </div>
-                <div class="carousel-item">
-                  <img src="${selected.images[2]}" class="w-auto d-inline"  alt="IMG" >
-                </div>
-                <div class="carousel-item">
-                  <img src="${selected.images[3]}" class="w-auto d-inline"  alt="IMG" >
-                </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
+          <section class="container w-50">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" id="mod-content"> </div>
           </section>
+          <div class="carousel-indicators" id="indicators"></div>
         </div>
         <div class="modal-footer container">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -61,19 +43,34 @@ class Modal extends HTMLElement {
         </div>
       </div>
     </div>
-  </div>`
+  </div>`; 
+  let demop = document.getElementById("mod-content");
+ let demob = document.getElementById("indicators");
+  for (var key in selected.images) {
+    key=parseInt(key)
+    if(key===1){
+      demob.innerHTML+=("<button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='" + (key-1) + "' class='item active' style='background-image:url(" +selected.images[key] +")'  aria-current='true' aria-label='Slide "+ key+"'></button>");
+      demop.innerHTML+=("<div class='carousel-item active'><img class='w-100 d-block' src='" +selected.images[key] + "' alt='img'></div>");
+    }else{
+      demob.innerHTML+=("<button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='" + (key-1) + "' class='item' style='background-image:url(" +selected.images[key] +")'   aria-current='true' aria-label='Slide "+ key+"'></button>");
+      demop.innerHTML+=("<div class='carousel-item'><img class='w-100 d-block' src='" +selected.images[key] + "' alt='img'></div>");
+    }
+  }     
   var exampleModal = document.getElementById('exampleModal');
   let myModal  = bootstrap.Modal.getOrCreateInstance(exampleModal) // Returns a Bootstrap modal instance
   myModal.show();
 
 
+  var myCarousel = document.querySelector('#carouselExampleIndicators')
+  var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 100,
+  pause: true,
+  nextWhenVisible: true
+})
       
   }
 
 }
-
-
-
 
 
   
