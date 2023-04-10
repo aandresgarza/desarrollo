d894938ee8cec66c215a0ee547a83a5de6c735e1
Antonio <aadresgarza@outlook.es>
2023-04-10 15:46:08 +0200

    add content

--- components/cs-modal.js
+++ components/cs-modal.js
@@ -38,7 +38,7 @@ class Modal extends HTMLElement {
                               <a  href="#" class="d-flex align-items-center text-decoration-none text-dark gap-1" data-bs-dismiss="modal" aria-label="Close">
                                 <span class="text-primary-app">&larr;</span> Ver artículos
                               </a>
-                                <img src="https://aandresgarza.github.io/gattue/img/cats-head.svg">
+                                <img src="https://aandresgarza.github.io/gattue/img/cats-head.svg" width="65">
                                 <span class="invisible">  Ver artículos</span>
                               </div>
                               <div class="modal-body container-sm">
@@ -48,18 +48,20 @@ class Modal extends HTMLElement {
                                 </section>
                                 <div id="indicators"></div>
                               </div>
-                              <div class="container-sm">
-                                <h5 class="modal-title" id="exampleModalLabel">${selected[0].title}</h5>
-                                <p>tellus ac scelerisque placerat, lorem mi consequat massa, a fermentum diam turpis a lectus. Quisque molestie sed tortor aliquam mollis. Fusce imperdiet vehicula faucibus. Sed id mauris justo.</p>
+                              <div class="container-sm mt-3">
+                              <h5 class="modal-title" id="exampleModalLabel">${selected[0].title}</h5>
+                                <p class="mt-3">${selected[0].description}</p>
                               </div>
-                              <div class="modal-footer position-fixed w-100 bg-white ">
-                              <div class="d-flex justify-content-between w-100">
-                                  <a  href="#" class="d-flex align-items-center text-decoration-none text-dark gap-1" data-bs-dismiss="modal" aria-label="Close">
+                              <div class="modal-footer position-fixed w-100 bg-light border-0">
+                              <div class="d-flex justify-content-between w-100 flex-wrap align-items-center gap-3">
+                                  <a  href="#" class="d-flex align-items-center text-decoration-none text-dark gap-1 w-down" data-bs-dismiss="modal" aria-label="Close">
                                     <span class="text-primary-app">&larr;</span> Ver artículos
                                   </a>
-                                  <div class="">
-                                    <button type="button" class="btn btn-secondary border-0" data-bs-dismiss="modal">Comprar en Vinted</button>
-                                    <button type="button" class="btn bg-primary-app text-white border-0">Comprar en Wallapop</button>
+                                  <strong>${selected[0].title}</strong>
+                                  <div class="d-flex gap-1 flex-wrap align-items-center">
+                                    <h3 class="mb-0 me-3">${selected[0].price}€</h3>
+                                    <button type="button" class="btn btn-secondary rounded-0" data-bs-dismiss="modal">Comprar en Vinted</button>
+                                    <button type="button" class="btn bg-primary-app text-white rounded-0">Comprar en Wallapop</button>
                                   </div>
                                   </div>
                               </div>
